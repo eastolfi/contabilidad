863f2b3c5460c8573ce97637e3e15f5faf6019f2
eastolfi <astolfi_wotc@hotmail.com>
2015-12-02 21:25:52 +0000

    New ddbb credentials

--- app/ddbb/ddbbConfig.js
+++ app/ddbb/ddbbConfig.js
@@ -20,8 +20,8 @@ module.exports = function() {
                 if (this.isPostgreSQL && process.env.NODE_ENV === 'production') {
                     var _host = 'ec2-54-247-170-228.eu-west-1.compute.amazonaws.com';
                     var _port = '5432';
-                    var _db = 'd1fhnj5hqlljv3';
-                    var _credentials = 'kjfzksezxibswp:xBUoEJ2RNDJLCZZa8OhU1-Sqff'
+                    var _db = 'dahrh9sqddtae';  //d1fhnj5hqlljv3
+                    var _credentials = 'uvbvqpqbfynwux:_WmUaQKnLeCiwN75gsaXF1au5o';     //kjfzksezxibswp:xBUoEJ2RNDJLCZZa8OhU1-Sqff
                     var _ssl = 'ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory';
                     
                     str = 'postgres://' + _credentials + '@' + _host + ':' + _port + '/' + _db + '?' + _ssl;
